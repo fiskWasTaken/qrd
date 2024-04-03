4da5f2e691fe504fe275f552ab89e978c9590b96
fisk <git@fisk.cc>
2024-04-03 23:07:07 +0100

    refactor: delete unused assets, add debounce

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <div class="main">
-      <div class="qrd-logo" />
+      <div class="qrd-logo"/>
       <div>
         <input type="text" class="qr-text" v-model="settings.qrText" placeholder="Type an URL or some text"/>
       </div>
       <div class="color-settings">
         <input type="color" v-model="settings.foreColor" title="Foreground"/>
-        <input type="color" v-model="settings.backColor" title="Background" />
+        <input type="color" v-model="settings.backColor" title="Background"/>
         <div class="setting">
           <input type="checkbox" :checked="settings.backAlpha === 0" id="transparency" @click="() => {
             settings.backAlpha = settings.backAlpha === 0 ? 255 : 0
@@ -28,17 +28,17 @@
         Background Alpha
         <input type="number" min="0" max="255" v-model="settings.backAlpha" placeholder="0-255"/>
       </div>
+      <div class="setting" title="Set the margin.">
+        Margin
+        <input type="number" min="0" v-model="settings.margin" placeholder="0"/>
+      </div>
       <div class="setting" title="Set the scale. Will not work if min width is set.">
         Scale
         <input type="number" min="0" v-model="settings.scale" placeholder="0"/>
       </div>
-      <div class="setting" title="Set the scale.">
-        Margin
-        <input type="number" min="0" v-model="settings.margin" placeholder="0"/>
-      </div>
       <div class="setting" title="Set the minimum width (px). Leave blank to use the scale option.">
-        Min Width
-        <input type="number" min="0" v-model="settings.minWidth" placeholder="unset"/>
+        Min Width (px)
+        <input type="number" min="0" v-model="settings.minWidth" placeholder="(Use Scale)"/>
       </div>
       <div class="setting" title="Forces the QR version (should keep this off, it is selected automatically).">
         <input type="checkbox" v-model="settings.forcedVersionEnabled" id="force-version"/>
@@ -49,8 +49,8 @@
         Mask Pattern
         <input type="number" min="0" max="7" v-model="settings.maskPattern" placeholder="0-7"/>
       </div>
-      <div class="setting" title="Error correction level (Default: Medium).">
-        Error correction level
+      <div class="setting" title="Error Correction Level (Default: Medium).">
+        Error Correction Level
         <select v-model="settings.errorCorrectionLevel">
           <option value="low">Low</option>
           <option value="medium">Medium</option>
@@ -76,7 +76,8 @@
         <span class="why-link" @click="expandWhy = !expandWhy">Why?</span>
       </div>
       <div>
-        <span class="qrd-inline">QRD</span> &copy; fisk, 2024. <a href="https://ko-fi.com/fisktech" target="_blank">Support me on Ko-fi!</a>
+        <span class="qrd-inline">QRD</span> &copy; fisk, 2024. <a href="https://ko-fi.com/fisktech" target="_blank">Support
+        me on Ko-fi!</a>
       </div>
       <div v-if="expandWhy" class="why">
         <p>
@@ -85,7 +86,8 @@
         </p>
         <p>
           If you type "QR code generator" into Google, you'll find a dozen sponsored results for sites requiring
-          sign-ups, etc. Most of these want a lot of money for little in return. The internet is so saturated with these websites that
+          sign-ups, etc. Most of these want a lot of money for little in return. The internet is so saturated with these
+          websites that
           finding one that isn't garbage is near impossible.
         </p>
         <p>I'm just a little person, and I just want to make a QR code sometimes. If you also like making QR codes, I
@@ -98,19 +100,23 @@
 
 <script setup>
 import * as QRCode from 'qrcode'
-import { onMounted, reactive, ref, watch} from "vue";
+import {onMounted, reactive, ref, watch} from "vue";
 
 const qrPreview = ref(null)
-
 const expandWhy = ref(false)
-
 const lastError = ref(null)
-
 const settings = reactive(getSettingsFromStorage())
-
 const renderedSize = ref(0)
 
-watch(settings, updateQR)
+let bounceTimer = null
+
+watch(settings, () => {
+  if (bounceTimer != null) {
+    clearTimeout(bounceTimer)
+  }
+
+  bounceTimer = setTimeout(updateQR, 100)
+})
 
 function getSettingsFromStorage() {
   const stored = localStorage.getItem("settings")
@@ -144,32 +150,24 @@ function putSettingsToStorage() {
   localStorage.setItem("settings", JSON.stringify(settings))
 }
 
-onMounted(() => {
-  updateQR()
-})
+onMounted(updateQR)
 
 function buildOptions() {
-  const options = {}
-
-  if (settings.forcedVersionEnabled) {
-    options.version = settings.forcedVersion
-  }
-
-  options.maskPattern = settings.maskPattern
-  options.errorCorrectionLevel = settings.errorCorrectionLevel
-  options.scale = settings.scale
-  options.width = settings.minWidth
-  options.margin = settings.margin
-
   const foreAlphaHex = Math.max(0, Math.min(255, settings.foreAlpha)).toString(16).padStart(2, '0')
   const backAlphaHex = Math.max(0, Math.min(255, settings.backAlpha)).toString(16).padStart(2, '0')
 
-  options.color = {
-    dark: `${settings.foreColor}${foreAlphaHex}`,
-    light: `${settings.backColor}${backAlphaHex}`
+  return {
+    maskPattern: settings.maskPattern,
+    errorCorrectionLevel: settings.errorCorrectionLevel,
+    scale: settings.scale,
+    width: settings.minWidth,
+    margin: settings.margin,
+    color: {
+      dark: `${settings.foreColor}${foreAlphaHex}`,
+      light: `${settings.backColor}${backAlphaHex}`
+    },
+    version: settings.forcedVersionEnabled ? settings.forcedVersion : null
   }
-
-  return options
 }
 
 function updateQR() {
@@ -192,16 +190,16 @@ function toPNG() {
   const opts = buildOptions()
   opts.type = 'image/png'
 
-  QRCode.toDataURL(settings.qrText, opts,  (e, data) => {
-    console.log(data)
+  QRCode.toDataURL(settings.qrText, opts).then(data => {
     download("qr.png", data)
   })
 }
 
 function toSVG() {
   const opts = buildOptions()
+  opts.type = 'svg'
 
-  QRCode.toString(settings.qrText, opts,  (e, data) => {
+  QRCode.toString(settings.qrText, opts).then(data => {
     download("qr.svg", `data:image/svg+xml;base64,${btoa(data)}`)
   })
 }
@@ -209,13 +207,18 @@ function toSVG() {
 </script>
 
 <style lang="scss">
+:root {
+  --accent: #517966;
+  --accent-light: #779b8a;
+  --accent-dark: #355d4a;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   margin-top: 60px;
-
   user-select: none;
 }
 
@@ -244,7 +247,7 @@ function toSVG() {
 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: start;
   flex-direction: column;
 
   margin-bottom: 1em;
@@ -263,15 +266,24 @@ function toSVG() {
 }
 
 .btn-save {
+  &:hover {
+    background-color: var(--accent);
+  }
+
+  &:active {
+    background-color: var(--accent-dark);
+  }
+
   @extend %border;
-  background-color: #779b8a;
-  border-color : #a1d2bb;
+  background-color: var(--accent-light);
+  border-color: var(--accent);
   color: #fff;
   font-weight: bold;
   padding: 0.5em 0.7em;
   line-height: 1;
   cursor: pointer;
   font-size: 1.05em;
+  transition: all 0.3s;
 }
 
 body {
@@ -294,20 +306,20 @@ input[type=text], input[type=number] {
 }
 
 .qr-text {
-  @extend %border;
-
-  font-size: 1.1em;
-  padding: 0.3em;
-  color: #666;
-  transition: all 0.3s;
-
   &:hover {
-    color: #000;
+    color: var(--accent-light);
   }
 
   &:focus {
-    color: #000;
+    color: var(--accent-dark);
   }
+
+  @extend %border;
+
+  font-size: 1.3em;
+  padding: 0.3em;
+  color: var(--accent);
+  transition: all 0.3s;
 
   text-align: center;
   width: 30em;
@@ -362,16 +374,16 @@ input[type=checkbox] {
 
 .why-link {
   cursor: pointer;
-  color: #517966;
+  color: var(--accent);
 }
 
 a {
-  color: #517966;
+  color: var(--accent);
   text-decoration: none;
 }
 
 .qrd-inline {
-  color: #517966;
+  color: var(--accent);
   font-weight: bold;
 }
 
