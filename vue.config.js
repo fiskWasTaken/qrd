545f9d5c0b49d52fe7886efae260e1da807e0c94
fisk <git@fisk.cc>
2024-04-12 05:09:02 +0100

    feat: pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  pwa: {
+    name: "QRD",
+    themeColor: "#f1ebea",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 })
